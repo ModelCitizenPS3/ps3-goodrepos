b688a806a33961da9947f0c4409d15af4f073759
Model Citizen <gshel054@gmail.com>
2022-02-14 00:56:05 -0500

    updated goodrepos.sh

--- goodrepos.sh
+++ goodrepos.sh
@@ -11,6 +11,7 @@ cp -v rpm-gpg/RPM-GPG-KEY-livna /etc/pki/rpm-gpg/
 cp -v rpm-gpg/RPM-GPG-KEY-remi /etc/pki/rpm-gpg/
 cp -v rpm-gpg/RPM-GPG-KEY-rpmfusion-free-fedora-12-primary /etc/pki/rpm-gpg/
 cp -v rpm-gpg/RPM-GPG-KEY-rpmfusion-nonfree-fedora-12-primary /etc/pki/rpm-gpg/
+
 rm -fv /etc/yum.repos.d/*
 cp -v yum.repos.d/fedora.repo /etc/yum.repos.d/
 cp -v yum.repos.d/fedora-updates.repo /etc/yum.repos.d/
@@ -23,10 +24,12 @@ cp -v yum.repos.d/rpmfusion-free-updates-testing.repo /etc/yum.repos.d/
 cp -v yum.repos.d/rpmfusion-nonfree.repo /etc/yum.repos.d/
 cp -v yum.repos.d/rpmfusion-nonfree-updates.repo /etc/yum.repos.d/
 cp -v yum.repos.d/rpmfusion-nonfree-updates-testing.repo /etc/yum.repos.d/
+
+echo "exclude=fedora-release rpmfusion-free-release rpmfusion-nonfree-release livna-release remi-release" >> /etc/yum.conf
+
 yum clean all
 yum makecache
-echo "exclude=fedora-release rpmfusion-free-release rpmfusion-nonfree-release livna-release remi-release" >> /etc/yum.conf
 yum -y update
 
-printf "\nREBOOT\n"
+printf "\nREBOOT\n\n"
 
